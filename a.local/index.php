1b7b4a2afc9aefeb05afb5594ad9f90fa413ead0
kurraz <elfd01@gmail.com>
2017-10-05 10:37:45 +0300

    init

---
+++ a.local/index.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+if(isset($_GET['auth']))
+{
+    $_SESSION['user_id'] = 123;
+}
+
+var_dump(session_id());
+var_dump($_SESSION['user_id'] ?? null);
+?>
+<script src="http://b.local/auth.php?id=<?= session_id() ?>"></script>
